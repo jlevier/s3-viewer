1259de04774162df7b18b63beae680a6e78f47c9
Jesse Levier <jlevier@gmail.com>
2023-05-22 10:16:49 -0400

    Final setup for basic files page

--- ui/files/files.go
+++ ui/files/files.go
@@ -1,8 +1,8 @@
 package files
 
 import (
+	"fmt"
 	"s3-viewer/api"
-	"s3-viewer/ui/list"
 	"s3-viewer/ui/types"
 
 	"github.com/aws/aws-sdk-go/service/s3"
@@ -20,7 +20,6 @@ type filesModel struct {
 	//spinner   spinner.Model
 	isLoading bool
 	//table     table.Model
-	listModel *list.Model
 }
 
 type getFilesMsg struct {
@@ -31,8 +30,6 @@ type getFilesMsg struct {
 func Init(m *types.UiModel) tea.Cmd {
 	cmds := make([]tea.Cmd, 0)
 	//cmds = append(cmds, model.spinner.Tick)
-
-	model.listModel = list.NewModel()
 
 	cmds = append(cmds, func() tea.Msg {
 		o, err := api.GetObjects(m.Session, m.GetCurrentBucket(), "/")
@@ -49,8 +46,6 @@ func Init(m *types.UiModel) tea.Cmd {
 
 func Update(m *types.UiModel, msg tea.Msg) tea.Cmd {
 	cmds := make([]tea.Cmd, 0)
-
-	cmds = append(cmds, list.Update(msg, model.listModel))
 
 	switch msg := msg.(type) {
 	case getFilesMsg:
@@ -75,7 +70,6 @@ func Update(m *types.UiModel, msg tea.Msg) tea.Cmd {
 }
 
 func View(m *types.UiModel) string {
-	// s := fmt.Sprintf("%s", model.directories)
-	// return fmt.Sprintf("In the files view for %s\n%s\n%s", s, m.GetCurrentBucket(), model.files)
-	return list.View(model.listModel)
+	s := fmt.Sprintf("%s", model.directories)
+	return fmt.Sprintf("In the files view for %s\n%s\n%s", s, m.GetCurrentBucket(), model.files)
 }
