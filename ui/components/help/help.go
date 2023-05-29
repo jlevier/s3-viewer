1d73deac349cffef92c2778047aebb283e65e152
Jesse Levier <jlevier@gmail.com>
2023-05-29 06:07:10 -0400

    Adding help to files view

--- ui/components/help/help.go
+++ ui/components/help/help.go
@@ -32,3 +32,22 @@ func GetBucketsHelp(hasActiveSearch bool) string {
 
 	return s.String()
 }
+
+func GetFilesHelp(hasActiveSearch bool) string {
+	var s strings.Builder
+
+	s.WriteString(styleKey.Render("\u2191"))
+	s.WriteString(styleDesc.Render(" up \u2022"))
+	s.WriteString(styleKey.Render(" \u2193"))
+	s.WriteString(styleDesc.Render(" down \u2022"))
+	s.WriteString(styleKey.Render(" /"))
+	s.WriteString(styleDesc.Render(" search \u2022"))
+	if hasActiveSearch {
+		s.WriteString(styleKey.Render("esc"))
+		s.WriteString(styleDesc.Render(" clear search \u2022"))
+	}
+	s.WriteString(styleKey.Render(" ctrl + c"))
+	s.WriteString(styleDesc.Render(" quit"))
+
+	return s.String()
+}
