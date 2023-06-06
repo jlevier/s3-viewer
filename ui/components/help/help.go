94925fe3a62ea9a3830c3031242bab4a5a74d055
Jesse Levier <jlevier@gmail.com>
2023-06-06 07:07:33 -0400

    Refactoring help

--- ui/components/help/help.go
+++ ui/components/help/help.go
@@ -15,45 +15,51 @@ var (
 			Foreground(lipgloss.Color("#5e5e5e"))
 )
 
+type helpItem struct {
+	key, desc string
+}
+
 func GetBucketsHelp() string {
-	var s strings.Builder
+	items := []helpItem{
+		{key: "\u2191", desc: "up"},
+		{key: "\u2193", desc: "down"},
+		{key: "enter", desc: "open folder"},
+		{key: "ctrl + c", desc: "quit"},
+	}
 
-	s.WriteString(renderHelpItem("\u2191", "up", true))
-	s.WriteString(renderHelpItem("\u2193", "down", true))
-	s.WriteString(renderHelpItem("enter", "open folder", true))
-	s.WriteString(renderHelpItem("ctrl + c", "quit", false))
-
-	return s.String()
+	return renderHelpItems(items)
 }
 
 func GetFilesHelp(filterPromptVisible bool, currentFilter string) string {
-	var s strings.Builder
-
+	items := make([]helpItem, 0)
 	if !filterPromptVisible {
-		s.WriteString(renderHelpItem("\u2191", "up", true))
-		s.WriteString(renderHelpItem("\u2193", "down", true))
-		s.WriteString(renderHelpItem("enter", "open folder", true))
-		s.WriteString(renderHelpItem("/", "filter", true))
+		items = append(items, helpItem{key: "\u2191", desc: "up"})
+		items = append(items, helpItem{key: "\u2193", desc: "down"})
+		items = append(items, helpItem{key: "enter", desc: "open folder"})
+		items = append(items, helpItem{key: "/", desc: "filter"})
 	}
 
 	if filterPromptVisible {
-		s.WriteString(renderHelpItem("enter", "apply filter", true))
-		s.WriteString(renderHelpItem("esc", "exit filter", true))
+		items = append(items, helpItem{key: "enter", desc: "apply filter"})
+		items = append(items, helpItem{key: "esc", desc: "exit filter"})
 	} else if currentFilter != "" {
-		s.WriteString(renderHelpItem("esc", "clear filter", true))
+		items = append(items, helpItem{key: "esc", desc: "clear filter"})
 	}
 
-	s.WriteString(renderHelpItem("ctrl + c", "quit", false))
+	items = append(items, helpItem{key: "ctrl + c", desc: "quit"})
 
-	return s.String()
+	return renderHelpItems(items)
 }
 
-func renderHelpItem(key, desc string, separator bool) string {
+func renderHelpItems(items []helpItem) string {
 	var s strings.Builder
-	s.WriteString(styleKey.Render(key))
-	s.WriteString(styleDesc.Render(fmt.Sprintf(" %s", desc)))
-	if separator {
-		s.WriteString(styleDesc.Render(" \u2022 "))
+
+	for i, h := range items {
+		s.WriteString(styleKey.Render(h.key))
+		s.WriteString(styleDesc.Render(fmt.Sprintf(" %s", h.desc)))
+		if i < len(items)-1 {
+			s.WriteString(styleDesc.Render(" \u2022 "))
+		}
 	}
 
 	return s.String()
