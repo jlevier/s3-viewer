a8dc99788481c64d6f4045e8f7a73ed2938dc63a
Jesse Levier <jlevier@gmail.com>
2023-06-07 07:24:12 -0400

    Breaking up table update and view components into separate files

--- ui/components/table/table.go
+++ ui/components/table/table.go
@@ -1,9 +1,7 @@
 package table
 
 import (
-	"fmt"
 	"os"
-	"strings"
 
 	spin "s3-viewer/ui/components/spinner"
 
@@ -47,176 +45,6 @@ func (m *Model) SetHasNextPage(hasNextPage bool) {
 
 func (m *Model) SetFooterInfo(f string) {
 	m.footerInfo = f
-}
-
-func (m *Model) View() string {
-	filter := m.renderFilter()
-	h := m.renderHeader()
-	r := m.renderRows()
-	f := m.renderFooter()
-
-	j := borderStyle.Render(
-		lipgloss.JoinVertical(lipgloss.Center, h, r, f))
-	jf := lipgloss.JoinVertical(lipgloss.Center, filter, j)
-
-	return jf
-}
-
-func (m *Model) renderFilter() string {
-	if m.isFilterVisible {
-		return filterWrapperStyle.Render(m.filterInput.View())
-	}
-
-	return ""
-}
-
-func (m *Model) renderHeader() string {
-	s := make([]string, len(m.columns))
-
-	for _, c := range m.columns {
-		style := headerRowStyle.Copy().
-			Width(c.Width)
-		s = append(s, style.Render(strings.ToUpper(c.Name)))
-	}
-
-	return lipgloss.JoinHorizontal(lipgloss.Center, s...)
-}
-
-func (m *Model) renderRows() string {
-	lastRow := m.getVisibleRowCount()
-
-	if lastRow < 0 {
-		return ""
-	}
-
-	s := make([]string, lastRow)
-
-	index := 0
-	for i := m.firstVisibleRow; i < lastRow+m.firstVisibleRow; i++ {
-		row := make([]string, len(m.columns))
-
-		for j, c := range m.columns {
-			row[j] = m.renderColumn(m.data[i][j], c, i, j)
-		}
-
-		s[index] = lipgloss.JoinHorizontal(lipgloss.Center, row...)
-		index++
-	}
-
-	_, height, _ := term.GetSize(int(os.Stdout.Fd()))
-	h := lipgloss.NewStyle().Height(height - 10)
-
-	return h.Render(lipgloss.JoinVertical(lipgloss.Center, s...))
-}
-
-func (m *Model) renderColumn(data string, c Column, currentRow, currentCol int) string {
-	style := lipgloss.NewStyle().Width(c.Width)
-	if currentRow == m.highlightedRowIndex {
-		style = highlightedRowStyle.Copy().Width(c.Width)
-	}
-	if currentCol == 0 {
-		style = style.Copy().Padding(0, 0, 0, 1)
-	} else if currentCol == len(m.columns)-1 {
-		style = style.Copy().Padding(0, 1, 0, 0)
-	}
-
-	dataFinal := data
-
-	// if data has folder path, strip off all folders but the last
-	folders := strings.Split(dataFinal, "/")
-	if len(folders) > 1 {
-		// path ended with / so last item is empty
-		if folders[len(folders)-1] == "" {
-			dataFinal = folders[len(folders)-2]
-		} else {
-			dataFinal = folders[len(folders)-1]
-		}
-	}
-
-	// If data is too large for the column, to prevent wrapping, truncate and add ellipses
-	calc := c.Width - 5
-	if len(dataFinal) > calc && calc > 0 {
-		dataFinal = fmt.Sprintf("%s...", dataFinal[:calc])
-	}
-
-	return style.Render(dataFinal)
-}
-
-func (m *Model) renderFooter() string {
-	width := 0
-	for _, w := range m.columns {
-		width += w.Width
-	}
-
-	var left strings.Builder
-	var right strings.Builder
-
-	left.WriteString(footerPrefixStyle.Render(" .. "))
-	left.WriteString(footerPathStyle.Render(m.footerInfo))
-
-	if m.isLoading {
-		right.WriteString(footerLoadingIconStyle.Render(m.spinner.View()))
-		right.WriteString(footerLoadingTextStyle.Render("loading"))
-	}
-
-	if m.currentFilter != "" {
-		right.WriteString(footerFilterStyle.Render(fmt.Sprintf("\uf002 %s", m.currentFilter)))
-	}
-
-	right.WriteString(m.renderPagingFooter())
-
-	right.WriteString(footerNavStyle.Render(m.renderNavFooter()))
-
-	right.WriteString(footerPosStyle.Render(fmt.Sprintf("%v/%v", m.highlightedRowIndex+1, len(m.data))))
-
-	rightStyle := footerStyle.Copy().
-		AlignHorizontal(lipgloss.Right).
-		Width(width / 2)
-
-	leftStyle := footerStyle.Copy().
-		Width(width / 2)
-
-	leftFinal := leftStyle.Render(left.String())
-	rightFinal := rightStyle.Render(right.String())
-
-	return lipgloss.JoinHorizontal(lipgloss.Bottom, leftFinal, rightFinal)
-}
-
-func (m *Model) renderNavFooter() string {
-	var nav strings.Builder
-
-	if m.highlightedRowIndex > 0 {
-		nav.WriteString("\uf062") // up arrow
-	} else {
-		nav.WriteString(" ")
-	}
-
-	nav.WriteString(" ")
-
-	if m.highlightedRowIndex < len(m.data)-1 {
-		nav.WriteString("\uf063") // down arrow
-	} else {
-		nav.WriteString(" ")
-	}
-
-	return nav.String()
-}
-
-func (m *Model) renderPagingFooter() string {
-	chars := make([]string, 0)
-
-	chars = append(chars, "\uf405")
-	chars = append(chars, fmt.Sprintf("%v", m.currentPageIndex+1))
-
-	if m.currentPageIndex > 0 {
-		chars = append(chars, "\uf04a") // left arrow
-	}
-
-	if m.hasNextPage {
-		chars = append(chars, "\uf04e") // right arrow
-	}
-
-	return footerPagingStyle.Render(strings.Join(chars, " "))
 }
 
 func (m *Model) GetHighlightedRow() *Row {
@@ -265,89 +93,28 @@ func (m *Model) Update(msg tea.Msg) (*Model, tea.Cmd) {
 	case tea.KeyMsg:
 		switch msg.String() {
 		case "up":
-			if m.highlightedRowIndex > 0 && !m.isFilterVisible {
-				m.highlightedRowIndex--
-			}
-
-			// See if you are now above the first visible row and need to shift displayed rows down by one
-			if m.highlightedRowIndex < m.firstVisibleRow {
-				m.firstVisibleRow--
-			}
+			m.handleUpKey()
 
 		case "down":
-			if m.highlightedRowIndex < len(m.data)-1 && !m.isFilterVisible {
-				m.highlightedRowIndex++
-			}
-
-			// See if you're past the end and need to shift the displayed rows up by one
-			if m.highlightedRowIndex > m.getVisibleRowCount()+m.firstVisibleRow-1 {
-				m.firstVisibleRow++
-			}
+			m.handleDownKey()
 
 		case "right":
-			if m.hasNextPage {
-				m.isLoading = true
-				cmds = append(
-					cmds,
-					func() tea.Msg {
-						m.currentPageIndex++
-						return NextPageMsg{
-							CurrentPageIndex: m.currentPageIndex - 1,
-						}
-					})
-			}
+			m.handleRightKey(&cmds)
 
 		case "left":
-			if m.currentPageIndex > 0 {
-				m.isLoading = true
-				cmds = append(
-					cmds,
-					func() tea.Msg {
-						m.currentPageIndex--
-						return PrevPageMsg{
-							CurrentPageIndex: m.currentPageIndex,
-						}
-					})
-			}
+			m.handleLeftKey(&cmds)
 
 		case "esc":
-			if m.isFilterVisible {
-				m.isFilterVisible = false
-			} else if m.currentFilter != "" {
-				m.currentFilter = ""
-				m.filterInput.SetValue("")
-
-				cmds = append(
-					cmds,
-					func() tea.Msg {
-						return FilterAppliedMsg{
-							Filter: m.currentFilter,
-						}
-					})
-			}
+			m.handleEscapeKey(&cmds)
 
 		case "/":
-			if m.hasFiltering {
-				if !m.isFilterVisible {
-					m.isFilterVisible = true
-					return m, m.filterInput.Focus()
-				}
+			rm, rc := m.handleSlashKey()
+			if rm != nil {
+				return rm, rc
 			}
 
 		case "enter":
-			if m.isFilterVisible {
-				m.isLoading = true
-				cmds = append(cmds, m.spinner.Tick)
-				m.currentFilter = m.filterInput.Value()
-				cmds = append(
-					cmds,
-					func() tea.Msg {
-						return FilterAppliedMsg{
-							Filter: m.currentFilter,
-						}
-					})
-				m.isFilterVisible = false
-			}
+			m.handleEnterKey(&cmds)
 		}
 	}
 
@@ -364,4 +131,17 @@ func (m *Model) Update(msg tea.Msg) (*Model, tea.Cmd) {
 	}
 
 	return m, tea.Batch(cmds...)
+}
+
+func (m *Model) View() string {
+	filter := m.renderFilter()
+	h := m.renderHeader()
+	r := m.renderRows()
+	f := m.renderFooter()
+
+	j := borderStyle.Render(
+		lipgloss.JoinVertical(lipgloss.Center, h, r, f))
+	jf := lipgloss.JoinVertical(lipgloss.Center, filter, j)
+
+	return jf
 }
--- ui/components/table/update.go
+++ ui/components/table/update.go
@@ -1,23 +1,97 @@
 package table
 
-// func (m *Model) handleUpKey() {
-// 	if m.highlightedRowIndex > 0 && !m.isFilterVisible {
-// 		m.highlightedRowIndex--
-// 	}
+import tea "github.com/charmbracelet/bubbletea"
 
-// 	// See if you are now above the first visible row and need to shift displayed rows down by one
-// 	if m.highlightedRowIndex < m.firstVisibleRow {
-// 		m.firstVisibleRow--
-// 	}
-// }
+func (m *Model) handleUpKey() {
+	if m.highlightedRowIndex > 0 && !m.isFilterVisible {
+		m.highlightedRowIndex--
+	}
 
-// func (m *Model) handleDownKey() {
-// 	if m.highlightedRowIndex < len(m.data)-1 && !m.isFilterVisible {
-// 		m.highlightedRowIndex++
-// 	}
+	// See if you are now above the first visible row and need to shift displayed rows down by one
+	if m.highlightedRowIndex < m.firstVisibleRow {
+		m.firstVisibleRow--
+	}
+}
 
-// 	// See if you're past the end and need to shift the displayed rows up by one
-// 	if m.highlightedRowIndex > m.getVisibleRowCount()+m.firstVisibleRow-1 {
-// 		m.firstVisibleRow++
-// 	}
-// }
+func (m *Model) handleDownKey() {
+	if m.highlightedRowIndex < len(m.data)-1 && !m.isFilterVisible {
+		m.highlightedRowIndex++
+	}
+
+	// See if you're past the end and need to shift the displayed rows up by one
+	if m.highlightedRowIndex > m.getVisibleRowCount()+m.firstVisibleRow-1 {
+		m.firstVisibleRow++
+	}
+}
+
+func (m *Model) handleRightKey(cmds *[]tea.Cmd) {
+	if m.hasNextPage {
+		m.isLoading = true
+		*cmds = append(
+			make([]tea.Cmd, 1),
+			func() tea.Msg {
+				m.currentPageIndex++
+				return NextPageMsg{
+					CurrentPageIndex: m.currentPageIndex - 1,
+				}
+			})
+	}
+}
+
+func (m *Model) handleLeftKey(cmds *[]tea.Cmd) {
+	if m.currentPageIndex > 0 {
+		m.isLoading = true
+		*cmds = append(
+			make([]tea.Cmd, 1),
+			func() tea.Msg {
+				m.currentPageIndex--
+				return PrevPageMsg{
+					CurrentPageIndex: m.currentPageIndex,
+				}
+			})
+	}
+}
+
+func (m *Model) handleEscapeKey(cmds *[]tea.Cmd) {
+	if m.isFilterVisible {
+		m.isFilterVisible = false
+	} else if m.currentFilter != "" {
+		m.currentFilter = ""
+		m.filterInput.SetValue("")
+
+		*cmds = append(
+			make([]tea.Cmd, 1),
+			func() tea.Msg {
+				return FilterAppliedMsg{
+					Filter: m.currentFilter,
+				}
+			})
+	}
+}
+
+func (m *Model) handleSlashKey() (*Model, tea.Cmd) {
+	if m.hasFiltering {
+		if !m.isFilterVisible {
+			m.isFilterVisible = true
+			return m, m.filterInput.Focus()
+		}
+	}
+
+	return nil, nil
+}
+
+func (m *Model) handleEnterKey(cmds *[]tea.Cmd) {
+	if m.isFilterVisible {
+		m.isLoading = true
+		*cmds = append(make([]tea.Cmd, 1), m.spinner.Tick)
+		m.currentFilter = m.filterInput.Value()
+		*cmds = append(
+			make([]tea.Cmd, 1),
+			func() tea.Msg {
+				return FilterAppliedMsg{
+					Filter: m.currentFilter,
+				}
+			})
+		m.isFilterVisible = false
+	}
+}
